e4812a8ebcf6deb4209e573cffff3c6dc08be9b8
Станислав Горожанкин <sgorozhankin@gmail.com>
2018-11-27 10:27:10 +0200

    add old project

---
+++ ConsoleApp4/Cell.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public class Cell
+    {
+        public int X;
+        public int Y;
+        public int State; //0 - Dead, 1 - Life Yellow , 2 - Dying Red, 3 - Born Green
+        public int Generation;
+
+        public Cell(int x, int y, int state)
+        {
+            X = x;
+            Y = y;
+            State = state;
+            Generation = 0;
+        }
+
+        public void NextGeneration1step(Cell[] Field, int cellNumberInArray)
+        {
+            int LifeCount = 0;
+
+            int x = cellNumberInArray / Program.FieldX;
+            int y = cellNumberInArray % Program.FieldX;
+
+            for (var x1 = x - 1; x1 < x + 2; x1++)
+            {
+                for (var y1 = y - 1; y1 < y + 2; y1++)
+                {
+                    if (x1 == x && y1 == y)
+                        continue;
+
+                    int tmpX, tmpY;
+                    LoopBack(out tmpX, x1);
+                    LoopBack(out tmpY, y1);
+                    if (tmpX != 0)
+                        tmpX *= Program.FieldX;
+
+                    if (Field[tmpX + tmpY].State == 1 || Field[tmpX + tmpY].State == 2)
+                        LifeCount++;
+                }
+            }
+
+            switch (State)
+            {
+                case 0:
+                    {
+                        if (LifeCount == 3)
+                            State = 3;
+                        break;
+                    }
+                case 1:
+                    {
+                        if (LifeCount != 3 && LifeCount != 2)
+                            State = 2;
+                        break;
+                    }
+                case 2:
+                    {
+                        State = 0;
+                        break;
+                    }
+                case 3:
+                    {
+                        State = 1;
+                        break;
+                    }
+            }
+        }
+
+        public void NextGeneration2step()
+        {
+            switch (State)
+            {
+                case 2:
+                    {
+                        State = 0;
+                        break;
+                    }
+                case 3:
+                    {
+                        State = 1;
+                        break;
+                    }
+            }
+        }
+
+        private void LoopBack(out int tmpX, int x1)
+        {
+            switch (x1)
+            {
+                case -1:
+                    {
+                        tmpX = Program.FieldX - 1;
+                        break;
+                    }
+
+                case Program.FieldX:
+                    {
+                        tmpX = 0;
+                        break;
+                    }
+
+                default:
+                    {
+                        tmpX = x1;
+                        break;
+                    }
+            }
+        }
+    }
+
+
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        public const int FieldX = 60;//60; //60; 35
+                                     //  const int FieldY = 50;//230;//180; 80
+
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            Cell[] CellArray = new Cell[FieldX * FieldX];
+            Random rand = new Random();
+
+            int count = 0;
+            for (var x = 0; x < FieldX; x++)
+            {
+                for (var y = 0; y < FieldX; y++)
+                {
+                    CellArray[count] = new Cell(x, y, rand.Next(0, 2));
+                    count++;
+                }
+            }
+
+            ////Glider
+            //CellArray[1 * FieldX + 2].State = 1;
+            //CellArray[2 * FieldX + 3].State = 1;
+            //CellArray[3 * FieldX + 1].State = 1;
+            //CellArray[3 * FieldX + 2].State = 1;
+            //CellArray[3 * FieldX + 3].State = 1;
+
+            ////Glider2
+            //CellArray[2 * FieldX - 3].State = 1;
+            //CellArray[3 * FieldX - 4].State = 1;
+            //CellArray[4 * FieldX - 4].State = 1;
+            //CellArray[4 * FieldX - 3].State = 1;
+            //CellArray[4 * FieldX - 2].State = 1;
+
+            ////Glider3
+            //CellArray[(FieldX - 4) * FieldX + 1].State = 1;
+            //CellArray[(FieldX - 4) * FieldX + 2].State = 1;
+            //CellArray[(FieldX - 4) * FieldX + 3].State = 1;
+            //CellArray[(FieldX - 3) * FieldX + 3].State = 1;
+            //CellArray[(FieldX - 2) * FieldX + 2].State = 1;
+
+            ////Glider4
+            //CellArray[(FieldX - 3) * FieldX - 2].State = 1;
+            //CellArray[(FieldX - 3) * FieldX - 3].State = 1;
+            //CellArray[(FieldX - 3) * FieldX - 4].State = 1;
+            //CellArray[(FieldX - 2) * FieldX - 4].State = 1;
+            //CellArray[(FieldX - 1) * FieldX - 3].State = 1;
+
+            Console.SetCursorPosition(0, 0);
+            PrintField(CellArray);
+            Console.ReadKey();
+
+            while (true)
+            {
+                for (var x = 0; x < FieldX * FieldX; x++)
+                {
+                    CellArray[x].NextGeneration1step(CellArray, x);
+                }
+
+                Console.SetCursorPosition(0, 0);
+                PrintField(CellArray);
+                //    Thread.Sleep(200);
+
+                for (var x = 0; x < FieldX * FieldX; x++)
+                {
+                    CellArray[x].NextGeneration2step();
+                }
+
+                Console.SetCursorPosition(0, 0);
+                PrintField(CellArray);
+                //   Thread.Sleep(200);
+            }
+        }
+
+        static void PrintField(Cell[] Field)
+        {
+            Console.Write("┌");
+            for (var i = 0; i < FieldX * 2; i++)
+            {
+                Console.Write("─");
+            }
+            Console.WriteLine("┐");
+
+            for (var count = 0; count < FieldX * FieldX; count++)
+            {
+                if (count % FieldX == 0 && count != 0) Console.Write("│\n│");
+                if (count == 0) Console.Write("│");
+                switch (Field[count].State)
+                {
+                    case 0:
+                        {
+                            Console.Write("  ");
+                            break;
+                        }
+                    case 1:
+                        {
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.Write("■ ");
+                            Console.ResetColor();
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.Write("■ ");
+                            Console.ResetColor();
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write("■ ");
+                            Console.ResetColor();
+                            break;
+                        }
+                }
+                if (count == FieldX * FieldX - 1) Console.Write("│");
+            }
+
+            Console.WriteLine();
+            Console.Write("└");
+            for (var i = 0; i < FieldX * 2; i++)
+            {
+                Console.Write("─");
+            }
+            Console.Write("┘");
+        }
+    }
+}
